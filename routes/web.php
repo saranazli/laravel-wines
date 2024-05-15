5897629453448c3d8367b28fe4815c5a1423dd8e
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-05-15 16:01:16 +0200

    Creazione db e migrazione tabella wines

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class,'index'])->name('home');
 
-Route::get('/nuova-pagina', [PageController::class, 'newPage'])->name('nuova-pagina');
+Route::get('/wines', [PageController::class, 'wines'])->name('wines');
