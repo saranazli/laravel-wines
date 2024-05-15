18d9fa0a2413fd97efe182742f908ebef151276d
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-15 17:29:05 +0200

    [add] seeder

--- database/seeders/WinesTableSeeder.php
+++ database/seeders/WinesTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Wine;
+
 
 class WinesTableSeeder extends Seeder
 {
@@ -12,6 +14,19 @@ class WinesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data_string= file_get_contents('https://api.sampleapis.com/wines/reds');
+        @dd($data_string);
+        $data=json_decode($data_string, true);
+
+        foreach($data as $wine){
+           $new_wine= new Wine();
+           $new_wine->winery=$wine->winery;
+           $new_wine->wine=$wine->wine;
+           $new_wine->average=$wine->average;
+           $new_wine->reviews=$wine->reviews;
+           $new_wine->location=$wine->location;
+           $new_wine->image=$wine->image;
+           $new_wine->save();
+        }
     }
 }
